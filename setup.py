8500c394526a0f20c1e787ded0a1ef1aa0f99bd9
MikeSmithEU <github@mikesmith.eu>
2019-02-26 15:37:17 +0100

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
-import os
-import sys
 from setuptools import setup, find_packages
-
-if sys.version_info < (3, 5):
-    sys.exit('Sorry, Python < 3.5 is not supported')
-
-__version__ = '0.0.1'
-__author__ = 'VIAA'
-__name__ = 'viaastatus'
 
 with open('README.md') as f:
     long_description = f.read()
 
 
 setup(
-    name=__name__,
+    name='viaastatus',
     url='https://github.com/viaacode/status/',
-    version=__version__,
-    author=__author__,
+    version='0.0.1',
+    author='VIAA',
     author_email='support@viaa.be',
     descriptiona='Status services',
     long_description=long_description,
@@ -25,10 +16,10 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
     ],
-    python_requires='>=3.5',
+    python_requires='>=3.4',
     packages=find_packages("src"),
     package_dir={"": "src"},
-    package_data={'conferatur': ['api/templates/*.html']},
+    package_data={'viaastatus': ['server/static/*']},
     include_package_data=True,
     install_requires=[
        'Flask>=1.0.2',
