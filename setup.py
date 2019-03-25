1b0270b3ed7256db2b1ae6b01c12dc6233f3aa89
MikeSmithEU <github@mikesmith.eu>
2019-03-25 17:11:08 +0100

    bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 setup(
     name='viaastatus',
     url='https://github.com/viaacode/status/',
-    version='0.0.2',
+    version='0.0.3',
     author='VIAA',
     author_email='support@viaa.be',
     descriptiona='Status services',
