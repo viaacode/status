34a2365ab8d07e03c9afd6ef740d11417cbb4525
MikeSmithEU <github@mikesmith.eu>
2019-03-25 13:07:29 +0100

    add support for gunicorn to run script

--- run.sh
+++ run.sh
@@ -3,5 +3,10 @@
 PORT=${PORT:-8080}
 PROCESSES=${PROCESSES:-4}
 THREADS=${THREADS:-2}
+STRATEGY=${STRATEGY:-uswgi}
 
-uwsgi --http :"$PORT" --wsgi-file src/viaastatus/server/wsgi.py --callable application --processes "$PROCESSES" --threads "$THREADS"
+if [[ "$STRATEGY" == "gunicorn" ]]; then
+   gunicorn viaastatus.server.wsgi:application --bind 0.0.0.0:"$PORT" --workers "$PROCESSES"
+else
+   uwsgi --http :"$PORT" --wsgi-file src/viaastatus/server/wsgi.py --callable application --processes "$PROCESSES" --threads "$THREADS"
+fi
